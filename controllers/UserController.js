2fc3171713f3c20498fab133c5ff2d8d095e99ae
ItSalman99 <salmanabbas985@gmail.com>
2024-05-28 01:31:48 +0500

    Practicing

---
+++ controllers/UserController.js
@@ -0,0 +1,55 @@
+const { response } = require("express");
+const UserModel = require("../models/User");
+const User = require("../models/User");
+
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await UserModel.find();
+
+    return res.status(200).json({
+      status: true,
+      data: users,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      status: false,
+      message: err.message,
+    });
+  }
+};
+
+exports.storeNewUser = async (req, res) => {
+  // Ensure req.body exists and contains the necessary fields
+
+  if (!req.body || !req.body.name || !req.body.email || !req.body.password) {
+    return res.status(400).json({
+      status: false,
+      message:
+        "name, email, and password are required fields in the request body.",
+    });
+  }
+
+  // Extract fields from req.body
+  const { name, email, password } = req.body;
+
+  // Create a new user instance
+  const user = new User({
+    name,
+    email,
+    password,
+  });
+
+  try {
+    const newUser = await user.save();
+
+    return res.status(200).json({
+      status: true,
+      data: newUser,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      status: false,
+      message: err.message,
+    });
+  }
+};
---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const UserController = require("./controllers/UserController");
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+mongoose.connect("mongodb://localhost:27017/practice", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Connected to MongoDB");
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello Node!");
+});
+
+// Routes
+app.get("/api/users", UserController.getAllUsers);
+app.post("/api/users/store", UserController.storeNewUser);
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
